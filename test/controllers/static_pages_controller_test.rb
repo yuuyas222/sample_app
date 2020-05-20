56341ed25822213f0532cc84e39cafc04d8317e1
yuuyas222 <yuuyas222@gmail.com>
2020-05-20 07:43:29 +0000

    [Finish] static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -9,6 +15,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+  
+  test "should get cantact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
